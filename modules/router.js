7bd559f1df1afb8ff030eb586d6976bfc2061d98
sebafudi <sebastianfudalej@gmail.com>
2021-06-28 18:43:01 +0200

    fix: spotify login on different port

--- modules/router.js
+++ modules/router.js
@@ -2,19 +2,18 @@ const config = require("./config")
 
 class Router {
   route(request, response) {
-    let url = new URL(request.url, request.protocol + '://' + request.headers.host)
+    let url = new URL(request.url, request.protocol + '://' + request.headers.host + "/")
     console.log(url.pathname)
     if (url.pathname === "/") {
       response.writeHead(200, { "Content-Type": "text/html" })
       response.write("<a href=\"/login\">login</a>")
       response.end()
     } else if (url.pathname === "/login") {
-      const hostname = `http://localhost:${config.port}/`
       const authorization =
         'https://accounts.spotify.com/authorize'
         + '?response_type=code'
         + '&client_id=' + config.spotify_client_id
-        + '&redirect_uri=' + encodeURIComponent(hostname + 'callback/')
+        + '&redirect_uri=' + encodeURIComponent(config.callback)
       response.writeHead(307, { "Location": authorization })
       response.end()
     } else if (url.pathname === "/callback/") {
