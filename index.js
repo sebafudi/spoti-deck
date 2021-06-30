2b1461ace9b5700cc58b43e4022b3414a0b32408
sebafudi <sebastianfudalej@gmail.com>
2021-06-30 18:47:06 +0200

    refactor: made router easier to change

--- index.js
+++ index.js
@@ -2,8 +2,21 @@ const http = require("http")
 const config = require("./modules/config")
 const Router = require("./modules/router")
 
-router = new Router();
+router = Router();
+
+router.addRoute("/", "<a href=\"/login\">login</a>")
+router.addRoute("/login", "login")
+router.addRoute("/callback/", "<a href=\"/\">main</a>")
 
 http.createServer(function (request, response) {
-  router.route(request, response)
+  router.route(request, response, (req, res, url) => {
+    if (url.pathname === "/login") {
+      const authorization =
+        'https://accounts.spotify.com/authorize'
+        + '?response_type=code'
+        + '&client_id=' + config.spotify_client_id
+        + '&redirect_uri=' + encodeURIComponent(config.callback)
+        res.writeHead(307, { "Location": authorization })
+    }
+  })
 }).listen(config.port);
--- modules/router.js
+++ modules/router.js
@@ -1,27 +1,27 @@
-const config = require("./config")
+module.exports = createApplication
 
-class Router {
-  route(request, response) {
-    let url = new URL(request.url, request.protocol + '://' + request.headers.host + "/")
-    console.log(url.pathname)
-    if (url.pathname === "/") {
-      response.writeHead(200, { "Content-Type": "text/html" })
-      response.write("<a href=\"/login\">login</a>")
-      response.end()
-    } else if (url.pathname === "/login") {
-      const authorization =
-        'https://accounts.spotify.com/authorize'
-        + '?response_type=code'
-        + '&client_id=' + config.spotify_client_id
-        + '&redirect_uri=' + encodeURIComponent(config.callback)
-      response.writeHead(307, { "Location": authorization })
-      response.end()
-    } else if (url.pathname === "/callback/") {
-      response.writeHead(200, { "Content-Type": "text/html" })
-      response.write("<a href=\"/\">main</a>")
-      response.end();
+function createApplication() {
+  let routes = [];
+  return {
+    route: (req, res, n) => {
+      let url = new URL(req.url, req.protocol + '://' + req.headers.host + "/")
+      res.writeHead(200, { "Content-Type": "text/html" })
+      let flag = 0;
+      n(req, res, url)
+      routes.forEach(path => {
+        console.log(path.path)
+        if (path.path == url.pathname) {
+          res.write(path.text)
+          flag = 1;
+        }
+      })
+      if (flag === 0) {
+        res.write("404");
+      }
+      res.end()
+    },
+    addRoute: (path, text) => {
+      routes.push({ path, text })
     }
   }
 }
-
-module.exports = Router
