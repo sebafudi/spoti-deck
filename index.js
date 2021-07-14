1a8efd1cb6cf4deea66ddc8b7bb74c260ca991cd
sebafudi <sebastianfudalej@gmail.com>
2021-07-14 17:18:16 +0200

    feat: add next() to pass control to next match

--- index.js
+++ index.js
@@ -6,46 +6,43 @@ const Spotify = require('./modules/spotify')
 router = Router()
 spotify = Spotify(config.spotify_client_secret, config.callback, config.spotify_client_id)
 
-router.addRoute('/', '<a href="/login">login</a>')
-router.addRoute('/login', 'login')
-router.addRoute('/callback/', '<a href="/">main</a>')
+router.addRoute('/', ({res, next}) => {
+  res.write('<a href="/login">login</a>')
+  next()
+})
+router.addRoute('/login', ({res, next}) => {
+  let scopes = 'user-read-currently-playing';
+  const authorization =
+    'https://accounts.spotify.com/authorize'
+    + '?response_type=code'
+    + '&client_id=' + config.spotify_client_id
+    + (scopes ? '&scope=' + encodeURIComponent(scopes) : '')
+    + '&redirect_uri=' + encodeURIComponent(config.callback)
+  res.writeHead(307, { 'Location': authorization })
+  next()
+})
+router.addRoute('/callback/', ({res, next, url}) => {
+  spotify.handleAccessToken(url.searchParams.get('code'))
+    .then(x => {
+      spotify.makeRequest('https://api.spotify.com/v1/me/player/currently-playing', x.access_token).then((xx) => {
+        let data = JSON.parse(xx.body)
+        res.write('<head>')
+        res.write('<meta charset="UTF-8">')
+        res.write('</head>')
+        res.write('<body>')
+        res.write('<a href="/">main</a><br />')
+        res.write(`Now playing: <b>${data.item.name}</b> by <b>${data.item.artists[0].name}</b>`)
+        res.write('</body>')
+        next()
+      })
+    })
+    .catch(err => {
+      console.log("error getting access token")
+      res.write('<a href="/">main</a><br />')
+      next()
+    })
+})
 
 http.createServer(function (request, response) {
-  router.route(request, response, (req, res, url) => {
-    return new Promise(resolve => {
-      // resolve()
-      if (url.pathname === '/login') {
-        let scopes = 'user-read-currently-playing';
-        const authorization =
-          'https://accounts.spotify.com/authorize'
-          + '?response_type=code'
-          + '&client_id=' + config.spotify_client_id
-          + (scopes ? '&scope=' + encodeURIComponent(scopes) : '')
-          + '&redirect_uri=' + encodeURIComponent(config.callback)
-        res.writeHead(307, { 'Location': authorization })
-        resolve()
-      } else if (url.pathname === '/callback/') {
-        spotify.handleAccessToken(url.searchParams.get('code'))
-          .then(x => {
-            console.log(x)
-            spotify.makeRequest('https://api.spotify.com/v1/me/player/currently-playing', x.access_token).then((xx) => {
-              let data = JSON.parse(xx.body)
-              res.write('<head>')
-              res.write('<meta charset="UTF-8">')
-              res.write('</head>')
-              res.write('<body>')
-              res.write(`Now playing: <b>${data.item.name}</b> by <b>${data.item.artists[0].name}</b><br />`)
-              res.write('</body>')
-              resolve()
-            })
-          })
-          .catch(err => {
-            console.log("error getting access token")
-            resolve()
-          })
-      } else {
-        resolve()
-      }
-    })
-  })
+  router.route(request, response)
 }).listen(config.port);
--- modules/router.js
+++ modules/router.js
@@ -3,25 +3,26 @@ module.exports = createApplication
 function createApplication() {
   let routes = [];
   return {
-    route: async (req, res, n) => {
+    route: async (req, res) => {
       let url = new URL(req.url, req.protocol + '://' + req.headers.host + "/")
       res.writeHead(200, { "Content-Type": "text/html" })
-      let flag = 0;
-      await n(req, res, url)
-      routes.forEach(path => {
+      let flag = 0
+      for (path of routes) {
         if (path.path == url.pathname) {
           console.log(path.path)
-          res.write(path.text)
+          await new Promise((next) => {
+            path.func({req, res, url, next})
+          })
           flag = 1;
         }
-      })
+      }
       if (flag === 0) {
-        res.write("404");
+        res.write("404")
       }
       res.end()
     },
-    addRoute: (path, text) => {
-      routes.push({ path, text })
+    addRoute: (path, func) => {
+      routes.push({ path, func })
     }
   }
 }
